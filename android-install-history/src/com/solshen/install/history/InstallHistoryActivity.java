9263c2c1ac85d8e34d84630d45005dda7d9cee9d
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2011-10-15 07:59:26 -0700

    Initial import

---
+++ android-install-history/src/com/solshen/install/history/InstallHistoryActivity.java
@@ -0,0 +1,13 @@
+package com.solshen.install.history;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InstallHistoryActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
